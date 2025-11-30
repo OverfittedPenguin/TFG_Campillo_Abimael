dd6a296658512a83bf5e04886c8e0b99dcedb8a5
Abimael <campilloabimael@gmail.com>
2025-11-30 23:14:15 +0100

    New plot framework by subplots. All implemented.

--- code/main_IPOPT.py
+++ code/main_IPOPT.py
@@ -6,7 +6,6 @@ from simulation import Sim
 from atmosphere import Atmos
 from nonlinearprogramming import NLP_CRUISE
 from plotterfunction import Plotter
-import matplotlib.pyplot as plt
 
 ###########################################################
 ##                  USER CONFIGURATION                   ##
@@ -97,104 +96,8 @@ time = np.round(solver.stats()['t_proc_total'],3)
 x = sol['x'].full().flatten()
 t = np.linspace(0.0, sim.tF, sim.N)
 
-# States and controls storage vectors.
-x1 = []
-x2 = []
-x3 = []
-x4 = []
-x5 = []
-x6 = []
-x7 = []
-u1 = []
-u2 = []
-u1_0 = []
-u2_0 = []
-
-for k in range(sim.N):
-    # For each variable, the current value is retrived and 
-    # appended into the respecive storage array.
-
-    idx = 9*k
-    x1.append(x[idx])
-    x2.append(x[idx + 1])
-    x3.append(x[idx + 2])
-    x4.append(x[idx + 3])
-    x5.append(x[idx + 4])
-    x6.append(x[idx + 5])
-    x7.append(x[idx + 6])
-    u1.append(x[idx + 7])
-    u2.append(x[idx + 8])
-    u1_0.append(w0[idx + 7])
-    u2_0.append(w0[idx + 8])
-
-# Arrays conversion.
-x1 = np.array(x1)
-x2 = np.array(x2)
-x3 = np.array(x3)
-x4 = np.array(x4)
-x5 = np.array(x5)
-x6 = np.array(x6)
-x7 = np.array(x7)
-u1 = np.array(u1)
-u2 = np.array(u2)
-
-# FORCES RECOSNTRUCTION
-Fxb_A = []
-Fzb_A = []
-Fxb_T = []
-Fxb_W = []
-Fzb_W = []
-
-for k in range(len(x1)):
-    # Computation of atmosphere parameters,
-    # aerodynamic velocity and dynamic pressure.
-    rho = atmos.ISA_RHO(-x6[k])
-    ua, wa = x1[k] - sim.wind[0], x2[k] - sim.wind[1]
-    alpha = np.arctan2(wa, ua)
-    V = np.sqrt(ua**2 + wa**2)
-    q_bar = 0.5*rho*V**2
-
-    # AERODYNAMIC COEFFICIENTS AND FORCES
-    CL = aircraft.CL_0 + aircraft.CL_alpha * alpha + aircraft.CL_de * u2[k]
-    CD = aircraft.CD_0 + aircraft.K * CL**2
-    Rsb = np.array([
-        [np.cos(alpha), np.sin(alpha)],
-        [-np.sin(alpha), np.cos(alpha)]
-    ])
-    Cb_aero = Rsb.T @ np.array([-CD, -CL])
-    Fb_aero = q_bar * aircraft.S * Cb_aero
-    Fxb_A.append(Fb_aero[0])
-    Fzb_A.append(Fb_aero[1])
-
-    # PROPULSIVE FORCE
-    T_max,M_T_max = aircraft.PROPULSIVE_FORCES_MOMENTS(V,aircraft.RPM,rho,alpha)
-    Fxb_T.append(u1[k] * T_max)
-
-    # WEIGHT
-    Rhb = np.array([
-        [np.cos(x4[k]), np.sin(x4[k])],
-        [-np.sin(x4[k]), np.cos(x4[k])]
-    ])
-    Fb_grav = Rhb.T @ np.array([0.0, x7[k] * atmos.g])
-    Fxb_W.append(Fb_grav[0])
-    Fzb_W.append(Fb_grav[1])
-
-# Computation of negative Fxb.
-Fxb = [a + b for a,b in zip(Fxb_A,Fxb_W)]
-
-# AoA RECONSTRUCTION
-alpha = np.arctan2(x2 - sim.wind[1], x1 - sim.wind[0])
-
 # PLOTS
-
 path = os.path.join(os.getcwd(), "images", "benchmark")
 os.makedirs(path, exist_ok=True)
-Plotter.GENERATE_PLOT(t,np.column_stack((x1, x2)),["u","w"],["Time [s]", "Velocity [m/s]","Body velocities through time","VEL.png"],path)
-Plotter.GENERATE_PLOT(t,np.column_stack((x4, alpha)),[r"$\theta$",r"$\alpha$"],["Time [s]", "Angles [rad]","Pitch and AoA through time","ANGLES.png"],path)
-Plotter.GENERATE_PLOT(t,x7,"Mass",["Time [s]", "Mass [kg]","Aircraft's mass through time","MASS.png"],path) 
-Plotter.GENERATE_PLOT(t,np.column_stack((Fxb, Fxb_T)),["Aerodynamic + Weight", "Thrust"],["Time [s]", "Forces [N]","Body x-axis forces through time","FORCES_Xb.png"],path)
-Plotter.GENERATE_PLOT(t,np.column_stack((Fzb_A, Fzb_W)),["Aerodynamic", "Weight"],["Time [s]", "Forces [N]","Body z-axis forces through time","FORCES_Zb.png"],path)  
-Plotter.GENERATE_PLOT(t,np.column_stack((u1, u1_0)),[r"$\delta_T$", r"$\delta_T init.$"],["Time [s]", "TPS [-]","Throttle position through time","CONTROL_dT.png"],path)
-Plotter.GENERATE_PLOT(t,np.column_stack((u2, u2_0)),[r"$\delta_e$", r"$\delta_e init.$"],["Time [s]", "Elevator [rad]","Elevator deflection through time","CONTROL_de.png"],path)
-Plotter.GENERATE_PLOT(x5,-x6,"Trajectory",["Horizontal distance [m]", "Altitude AGL [m]","Aircraft's trajectory","TRAJECTORY.png"],path)
-Plotter.GENERATE_PLOT(np.linspace(1,iters+1,len(obj)),np.array(obj),"Objective cost",["Iterations [-]", "Cost objective [-]", "Cost evolution. Total computation time: " f"{time} s", "COST.png"], path)
+Plotter.GENERATE_RESULTS_PLOT(t,x,w0,aircraft,sim,path)
+Plotter.GENERATE_COST_PLOT(np.linspace(0,iters,len(obj)),np.array(obj),time,path)
--- code/nonlinearprogramming.py
+++ code/nonlinearprogramming.py
@@ -191,16 +191,16 @@ class NLP_CRUISE:
         ubg_path = []
 
         # Path constraints. Inequality constraints.
-        g_path.append(0.9*sim.Vtp - ca.sqrt(ua**2 + wa**2))
+        g_path.append(0.95*sim.Vtp - ca.sqrt(ua**2 + wa**2))
         lbg_path.append(-1e20)
         ubg_path.append(0)
-        g_path.append(ca.sqrt(ua**2 + wa**2) - 1.1*sim.Vtp)
+        g_path.append(ca.sqrt(ua**2 + wa**2) - 1.05*sim.Vtp)
         lbg_path.append(-1e20)
         ubg_path.append(0)   
 
         # TARGET ALTITUDE CONSTRAINT
-        href_l = -sim.w0[5] - 3.0
-        href_u = -sim.w0[5] + 3.0
+        href_l = -sim.w0[5] - 2.5
+        href_u = -sim.w0[5] + 2.5
         hi = -wi[5]
 
         # Path constraints. Inequality constraints.
@@ -269,26 +269,16 @@ class NLP_CRUISE:
         # Computation of trim condition.
         w0_trim_node = NLP_CRUISE.COMPUTE_TRIM(ac,at,sim)
         sim.w0 = w0_trim_node[:9]
-        print(sim.w0)
         
         # Reconstruction of full initial guess
         # as planar functions.
         w0_ls = []
-        w0_ls.append(w0_trim_node[0])
-        w0_ls.append(w0_trim_node[1])
-        w0_ls.append(w0_trim_node[2])
-        w0_ls.append(w0_trim_node[3])
-        w0_ls.append(w0_trim_node[4])
-        w0_ls.append(w0_trim_node[5])
-        w0_ls.append(w0_trim_node[6])
-        w0_ls.append(w0_trim_node[7])
-        w0_ls.append(w0_trim_node[8])
-        for k in range(1,N):
+        for k in range(N):
             w0_ls.append(w0_trim_node[0])
             w0_ls.append(w0_trim_node[1])
             w0_ls.append(w0_trim_node[2])
             w0_ls.append(w0_trim_node[3])
-            w0_ls.append(w0_ls[idx+4] + w0_trim_node[0]*np.cos(w0_trim_node[3])*dT)
+            w0_ls.append(w0_trim_node[0]*np.cos(w0_trim_node[3])*dT*k)
             w0_ls.append(w0_trim_node[5])
             w0_ls.append(w0_trim_node[6])
             w0_ls.append(w0_trim_node[7])
@@ -350,7 +340,7 @@ class NLP_CRUISE:
             # Weights assignation for gamma, gamma dot and controls.
             wg = 0.45
             wh = 0.45
-            wg_dot = 0.6
+            wg_dot = 0.60
             wdt = 0.60
             wde = 0.75
 
@@ -385,5 +375,5 @@ class NLP_CRUISE:
             hj = -wj[5]
 
             # COST FUCNTIONAL (Minimisation of gamma, gamma dot and controls)
-            J += dT/2 * (wg*(gi**2 + gj**2) / g_max**2 + wg_dot*(gi_dot**2 + gj_dot**2) / g_dot_max**2  + wh*((hi - href)**2 / href**2 + (hj - href)**2 / href**2)) + wde*ca.sumsqr((wj[8] - wi[8]) / (sim.dT*de_max)) + wdt*ca.sumsqr((wj[7] - wi[7]) / sim.dT)
+            J += dT/2 * (wg*(gi**2 + gj**2) / g_max**2 + wg_dot*(gi_dot**2 + gj_dot**2) / g_dot_max**2  + wh*((hi - href)**2 / href**2 + (hj - href)**2 / href**2)) + wde*(wj[8] - wi[8])**2 / (sim.dT*de_max**2) + wdt*(wj[7] - wi[7])**2 / sim.dT
         return J
