4fbc6db7833ed72e088d9f041c58ae29a5093379
Abimael <campilloabimael@gmail.com>
2025-12-08 20:51:08 +0100

    SImulation file and framework already changed to include all stages.

--- code/simulation.py
+++ code/simulation.py
@@ -8,11 +8,13 @@ class Sim:
             self,
             DT: float,
             INITIAL_STATE: list,
-            END_TIME_CONSTRAINTS: list,
-            ADDITIONAL_CONSTRAINTS: list,
-            ADDITIONAL_BOUNDS: list,
+            END_STATE: list,
             WIND_SPEED: list,
-            CRUISE_WEIGHTS:list,
+            TARGET_POINT: list,
+            MISSION_BOUNDS: list,
+            STG1_WEIGHTS:list,
+            STG2_WEIGHTS:list,
+            STG3_WEIGHTS:list,
             AIRCRAFT_FILE: str
         ):
             # PARAMETERS  
@@ -20,44 +22,50 @@ class Sim:
             self.N = int(1 / self.dT)
 
             self.x0_USER = np.array(INITIAL_STATE)
-            self.t0 = float(self.x0_USER[3])
-            self.x0 = np.zeros(11)
+            self.V0 = float(self.x0_USER[0])
+            self.theta0 = float(self.x0_USER[1])
+            self.t0 = float(self.x0_USER[2])
+            self.x0 = np.zeros(8)
             self.w0 = 0.0
 
-            self.wf = END_TIME_CONSTRAINTS
-
-            self.CONSTRAINTS = np.array(ADDITIONAL_CONSTRAINTS)
-            self.BOUNDS = np.array(ADDITIONAL_BOUNDS)
-            self.Vtp = self.CONSTRAINTS[0]
-            self.t_lb = self.BOUNDS[0]
-            self.t_ub = self.BOUNDS[1]
-            
-
-            self.cruise_wg = np.array(CRUISE_WEIGHTS)
+            self.wf = END_STATE
 
             self.wind = np.array(WIND_SPEED)
+
+            self.target = np.array(TARGET_POINT)
+            self.R_entry = float(self.target[0])
+            self.R_exit = float(self.target[1])
+            self.f_tp = float(self.target[2])
+            self.Vtp = float(self.target[3])
+
+            self.bounds = np.array(MISSION_BOUNDS)
+            self.lb = self.bounds[0]
+            self.ub = self.bounds[1]
+            
+            self.stg1_wg = np.array(STG1_WEIGHTS)
+            self.stg2_wg = np.array(STG2_WEIGHTS)
+            self.stg3_wg = np.array(STG3_WEIGHTS)
 
             self.Aircraft_file = AIRCRAFT_FILE
 
             # INITIAL STATE COMPUTATIONS
-            # Re-arranging of initial altitude AGL.
-            self.x0[6] = self.x0_USER[2] 
+            # Body velocities. Aerodynamic velocity.
+            u, w = self.V0 / np.cos(self.theta0), self.V0 / np.sin(self.theta0)
+            ua, wa = u - self.wind[0], w - self.wind[1]
+            V = np.sqrt(ua**2 + wa**2)
+            self.x0[0] = V
+            self.x0[1] = u
+            self.x0[2] = w
 
-            # Initial aerodynamic velocity.
-            ua, wa = self.x0_USER[0] - self.wind[0], self.x0_USER[1] - self.wind[1]
-            self.x0[2] = self.x0_USER[1]
-            self.x0[1] = self.x0_USER[0]
-            self.x0[0] = np.sqrt(ua**2 + wa**2)
-
-            # Initial position. Always zero.
-            self.x0[5] = 0.0
-
-            # Initial pitch rate. Always zero.
+            # Pitch rate. Always zero.
             self.x0[3] = 0.0
 
-            # Initial pitch. Equal to initial AoA.
-            self.x0[4] = np.abs(np.arctan2(wa,ua))
+            # Pitch. User input.
+            self.x0[4] = self.theta0
 
+            # Initial distance and altitude. Zero and target point altitude (altitude ub, cruise altitude).
+            self.x0[5] = 0.0
+            self.x0[6] = self.ub[3]
     
     @classmethod
     def from_json(cls, filepath: str) -> "Sim":
@@ -67,9 +75,9 @@ class Sim:
         
         # ESSENTIAL KEYS
         essential_keys = [
-            "DT", "INITIAL_STATE", "END_TIME_CONSTRAINTS",
-            "ADDITIONAL_CONSTRAINTS", "ADDITIONAL_BOUNDS",
-            "WIND_SPEED", "CRUISE_WEIGHTS", "AIRCRAFT_FILE"
+            "DT", "INITIAL_STATE", "END_STATE", "WIND_SPEED",
+            "TARGET_POINT", "MISSION_BOUNDS", "STG1_WEIGHTS",
+            "STG2_WEIGHTS", "STG3_WEIGHTS", "AIRCRAFT_FILE"
         ]
         missing = [k for k in essential_keys if k not in data]
         if missing:
